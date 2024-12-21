3d3191fa23d7d3dfcca1fce11cdc896527a79a18
super-daddy <abhishekanandsm0311@gmail.com>
2024-12-21 13:41:58 +0530

    working on cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -39,7 +39,7 @@ cartSummaryHTML +=  `
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -97,3 +97,10 @@ cartSummaryHTML +=  `
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
 console.log(cartSummaryHTML)
+
+document.querySelectorAll('.js-delete-link').forEach((link)=>{
+  link.addEventListener('click',()=>{
+    const productId =  link.dataset.productId;
+    console.log(productId);
+  })
+})
