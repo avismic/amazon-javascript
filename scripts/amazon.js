291a647a453e5ce5452ec9a8bf20ebb73b22ba8c
super-daddy <abhishekanandsm0311@gmail.com>
2024-12-28 19:14:45 +0530

    made checkout interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
 import {cart,addToCart} from '../data/cart.js'; 
 import {products} from '../data/products.js';
-import { formatCurency } from './utils/money.js';
+import { formatCurrency } from './utils/money.js';
 
 
 let productsHTML = '';
@@ -26,7 +26,7 @@ products.forEach((product)=>{
             </div>
 
             <div class="product-price">
-              ${formatCurency(product.priceCents)}
+              ${formatCurrency(product.priceCents)}
             </div>
 
             <div class="product-quantity-container">
